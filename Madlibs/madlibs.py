080020867987b59a4c313385a6ec2ce52e5696c9
MomenAhmed <mo2mination@gmail.com>
2023-07-08 15:55:53 +0300

    Add project directory

--- Madlibs/madlibs.py
+++
@@ -1,9 +0,0 @@
-name1 = input('Name #1: ')
-name2 = input('Name #2: ')
-adj = input('Adjective: ')
-noun = input('Noun: ')
-
-madlib = f'Under the moonlight, {name1} and {name2} shared a {adj} dance,\
-twirling and laughing as if they were the only ones in the {noun}.'
-
-print(madlib)
