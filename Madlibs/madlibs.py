562e499565e26b8fe2aa06345ae91f8ac36348e5
MomenAhmed <mo2mination@gmail.com>
2023-07-08 15:54:47 +0300

    Remake madlibs

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,9 @@
+name1 = input('Name #1: ')
+name2 = input('Name #2: ')
+adj = input('Adjective: ')
+noun = input('Noun: ')
+
+madlib = f'Under the moonlight, {name1} and {name2} shared a {adj} dance,\
+twirling and laughing as if they were the only ones in the {noun}.'
+
+print(madlib)
