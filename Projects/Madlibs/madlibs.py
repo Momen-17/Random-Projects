09b28df602de36afe531ed624c0b41363a5aa347
MomenAhmed <mo2mination@gmail.com>
2023-07-08 16:20:48 +0300

    Update madlib

--- Projects/Madlibs/madlibs.py
+++ Projects/Madlibs/madlibs.py
@@ -1,9 +1,28 @@
-name1 = input('Name #1: ')
-name2 = input('Name #2: ')
-adj = input('Adjective: ')
-noun = input('Noun: ')
+place = input('A Place: ')
+adj1 = input('Adjective #1: ')
+adj2 = input('Adjective #2: ')
+female = input('Female Celebrity')
+male = input('Male Celebrity')
+noun1 = input('Noun #1')
+noun2 = input('Noun #2')
+noun3 = input('Noun #3')
+plural1 = input('Plural Noun #1: ')
+plural2 = input('Plural Noun #2: ')
+plural3 = input('Plural Noun #3: ')
+plural4 = input('Plural Noun #4: ')
+profession = input('Plural Profession: ')
 
-madlib = f'Under the moonlight, {name1} and {name2} shared a {adj} dance,\
-twirling and laughing as if they were the only ones in the {noun}.'
-
-print(madlib)
+madlib = f'Albert Einstein, the son of {male} and {female},\
+was born in Ulm, Germany, in 1879. In 1902, he had a job\
+as assistant {noun1}.in the Swiss patent office and attended\
+the University of Zurich. There he began studying atoms, molecules,\
+and {plural1}. He developed the theory of\
+{adj1} relativity, which expanded the phenomena of sub-atomic\
+{plural2} and {adj2} magnetism. In 1921,\
+he won the Nobel prize for {plural3}.and was director of\
+theoretical physics at the Kaiser Wilhelm {noun2}.in Berlin.\
+In 1933, when Hitler became Chancellor of {place},\
+Einstein came to America to take a post at Princeton Institute for\
+{plural4}, where his theories helped America devise the first\
+atomic {noun3}. There is no question about it: Einstein was\
+one of the most brilliant {profession} of our time.'
